6500811195c9c367a4d7f7a9125a5cf1c6610190
Afrooz01 <farzanaafrooz@yahoo.com>
2023-06-15 00:10:56 -0400

    update my project

--- src/test/java/qa_DellStepDeff/DellStepDeffClass.java
+++ src/test/java/qa_DellStepDeff/DellStepDeffClass.java
@@ -1,5 +1,26 @@
 package qa_DellStepDeff;
 
-public class DellStepDeffClass {
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 
+public class DellStepDeffClass {
+	@Given("launch application <URL>")
+	public void launch_application_url() {
+	   
+	}
+
+	@Then("enter laptop in the search field")
+	public void enter_laptop_in_the_search_field() {
+	   
+	}
+
+	@Then("click search button")
+	public void click_search_button() {
+	    
+	}
+
+	@Then("verify user can search laptop")
+	public void verify_user_can_search_laptop() {
+	 
+	}
 }
